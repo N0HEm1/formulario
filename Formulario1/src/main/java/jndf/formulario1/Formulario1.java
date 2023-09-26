fd0389d0605ba4c7fb0efa37d98a03fcbf4c24c5
N0HEm1 <145424554+N0HEm1@users.noreply.github.com>
2023-09-26 08:37:14 -0600

    Add files via upload

---
+++ Formulario1/src/main/java/jndf/formulario1/Formulario1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package jndf.formulario1;
+import javax.swing.*;
+/**
+ *
+ * @author User
+ */
+public class Formulario1 extends JFrame{
+    public Formulario1(){
+         setLayout(null);
+    }
+
+        public static void main(String[] args) {
+            Formulario1 formulario1=new Formulario1();
+            formulario1.setBounds(10,20,400, 300);
+            formulario1.setVisible(true);
+            formulario1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }   
+   
+}
